1e0960ee2f4c1d66fb48d891314be1efe73d8b04
ayroid <ayushsk0000@gmail.com>
2023-09-12 09:56:37 +0530

    project initialized

---
+++ src/main/java/DevOps/App.java
@@ -0,0 +1,35 @@
+package DevOps;
+
+public class App {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) throws ArithmeticException {
+        return a / b;
+    }
+
+    public int power(int a, int b) {
+        int result = 1;
+        for (int i = 0; i < b; i++)
+            result *= a;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        App myCalc = new App();
+        System.out.println(myCalc.add(1, 2));
+        System.out.println(myCalc.subtract(1, 2));
+        System.out.println(myCalc.multiply(1, 2));
+        System.out.println(myCalc.divide(1, 2));
+        System.out.println(myCalc.power(1, 2));
+    }
+}
