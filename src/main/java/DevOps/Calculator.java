b8e068034f993e1ba7b2a1d0fe55d093271fe08f
ayroid <ayushsk0000@gmail.com>
2023-09-12 20:49:26 +0530

    Project updated

--- src/main/java/DevOps/Calculator.java
+++ src/main/java/DevOps/Calculator.java
@@ -17,19 +17,11 @@ public class Calculator {
         return a / b;
     }
 
-    public int power(int a, int b) {
-        int answer = 1;
-        for (int i = 0; i < b; i++)
-            answer *= a;
-        return answer;
-    }
-
     public static void main(String[] args) {
         Calculator myCalc = new Calculator();
         System.out.println(myCalc.add(1, 2));
         System.out.println(myCalc.subtract(1, 2));
         System.out.println(myCalc.multiply(1, 2));
         System.out.println(myCalc.divide(1, 2));
-        System.out.println(myCalc.power(1, 2));
     }
 }
